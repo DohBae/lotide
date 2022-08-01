9bec01cf3ade5bec78e1f16629bd5fead9e6d0f2
DohBae <bdoherty92@msn.com>
2022-08-01 23:23:52 +0000

    Wrote module.exports object using ES6 shorthand syntax instead of verbose syntax.

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+const assertArrayEqual = require('./assertArraysEqual');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  eqArrays: eqArrays,
-  assertEqual: assertEqual,
-  assertArrayEqual: assertArrayEqual
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertEqual,
+  assertArrayEqual
 };
+console.log(module.exports);
